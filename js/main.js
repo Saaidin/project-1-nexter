db100ee5bc9dfe7d5fabf9594eada00f604dde31
saaidin <dinme73@gmail.com>
2022-07-19 00:30:40 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+const menu_btn = document.querySelector('.menu-btn');
+	const mobile_menu = document.querySelector('.mobile-nav');
+
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+		mobile_menu.classList.toggle('is-active');
+	});
